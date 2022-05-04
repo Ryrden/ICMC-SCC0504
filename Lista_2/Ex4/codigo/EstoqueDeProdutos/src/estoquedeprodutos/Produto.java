ec3012ff466c48678b4017fc21c2e5aaa40ab878
Ryan <ryansateles@gmail.com>
2022-05-04 15:02:21 -0300

    codificação com base no UML

---
+++ Lista_2/Ex4/codigo/EstoqueDeProdutos/src/estoquedeprodutos/Produto.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package estoquedeprodutos;
+
+/**
+ *
+ * @author ryrden
+ */
+public class Produto {
+    protected int codigo;
+    protected float precoUnitario;
+    protected int qntEstoque;
+    protected String descricao;
+   
+    public Produto(int codigo, float precoUnitario, int qntEstoque, String descricao){
+        
+    }
+}
