f4ae78471f726095bf8f21a51046669a9cbd3b9d
silmarp <silmarjr2@gmail.com>
2022-05-08 18:18:36 -0300

    ex5 finalizado

---
+++ Lista_2/Ex5/SOjava/src/sojava/Driver.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package sojava;
+
+/**
+ *
+ * @author prod
+ */
+public interface Driver {
+    public boolean ligaDesliga();
+    public int ChecaStatus();
+    public void calibrar();
+}
---
+++ Lista_2/Ex5/SOjava/src/sojava/Teclado.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sojava;
+
+/**
+ *
+ * @author prod
+ */
+public class Teclado implements Driver{
+
+    @Override
+    public boolean ligaDesliga() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public int ChecaStatus() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void calibrar() {
+        System.out.println("Teclado calibrado"); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
