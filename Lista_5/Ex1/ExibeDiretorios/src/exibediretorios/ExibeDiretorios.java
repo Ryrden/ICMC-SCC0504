f1f434c17d9396001ba7b1af636f2ad1cf80a8fc
Ryan <ryansateles@gmail.com>
2022-05-31 20:45:43 -0300

    Ex1 Lista5

---
+++ Lista_5/Ex1/ExibeDiretorios/src/exibediretorios/ExibeDiretorios.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exibediretorios;
+
+/**
+ *
+ * @author ryan.souza
+ */
+public class ExibeDiretorios {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        soFiles.scanFiles("C:\\Users\\ryan.souza\\Desktop");
+    }
+    
+}
